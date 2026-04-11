9195f81940a3911535f62feb81ca3f7cbfd8c904
Ziyong <ziyongcheong@gmail.com>
2026-04-12 01:21:51 +0200

    update plugins to latest tag if available

--- functions/plugin_update.fish
+++ functions/plugin_update.fish
@@ -1,7 +1,8 @@
 function plugin_update
-    for plugin in $plugins
+    for i in (seq (count $plugins))
         # remove revision
-        set --local plugin_name (path basename $plugin | string split @)[1]
+        set --local repo (string split -- @ $plugins[$i])
+        set --local plugin_name (path basename $repo[1])
         set --local plugin_dir $_plugins_dir/$plugin_name
 
         if contains $plugin_name $plugins_pinned
@@ -11,7 +12,17 @@ function plugin_update
 
         echo Updating (set_color --bold)$plugin_name(set_color normal)
 
-        git -C $plugin_dir pull --quiet
+        if set --query $repo[2]
+            # get latest tag and update plugin name
+            set --function latest_tag (git -C $plugin_dir tag --sort -creatordate | head -n1)
+            set plugins[$i] $repo[1]@$latest_tag
+        else
+            # no revision, simply pull from HEAD
+            set --function latest_tag HEAD
+        end
+
+        git -C $plugin_dir fetch --quiet --depth 1 origin $latest_tag
+        git -C $plugin_dir checkout --quiet FETCH_HEAD
 
         for conf in $plugin_dir/conf.d/*.fish
             source $conf
